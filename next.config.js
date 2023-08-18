c2021a3ff686aa5de3d132d292ec025cd2b64ed8
Ron Nuss <shalom@itsron.space>
2023-08-18 15:09:06 +0300

    fix(next.config.js): images, asset, output

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /*
   images: {
     loader: "akamai",
     path: "",
   },
   assetPrefix: "./",
-  */
+  output: 'export',
 };
 
 module.exports = nextConfig;
